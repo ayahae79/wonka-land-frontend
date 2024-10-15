efef19ac1962e528ae1047d26ef31f3736c9522c
Dana <Danayousif867@gmail.com>
2024-10-15 14:36:06 +0300

    the functions to add game is done

--- src/pages/GameDetails.jsx
+++ src/pages/GameDetails.jsx
@@ -1,33 +1,38 @@
-
-import React, { useState, useEffect } from 'react';
-const BASE_URL = "http://localhost:3000";
+import axios from "axios"
+import React, { useState, useEffect } from "react"
+const BASE_URL = "http://localhost:3000"
 const GameDetails = ({ gameId }) => {
-  
   const [gameDetails, setGameDetails] = useState({})
   useEffect(() => {
     const fetchGameDetails = async () => {
       try {
-        const response = await fetch(`${BASE_URL}/games/${gameId}`);
-        const data = await response.json();
-        setGameDetails(data);
+        const response = await axios(`${BASE_URL}/game/games/${gameId}`)
+        const data =  response.data
+        console.log(response)
+        setGameDetails(data)
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-    };
-    fetchGameDetails();
-  }, [gameId]);
+    }
+    fetchGameDetails()
+  }, [gameId])
   return (
     <div>
       <h1>{gameDetails.name}</h1>
-      <img src={gameDetails.image} alt={gameDetails.title} style={{ width: '200px', height: '200px' }} />
+      <img
+        src={gameDetails.image}
+        alt={gameDetails.title}
+        style={{ width: "200px", height: "200px" }}
+      />
       <p>Age allowed: {gameDetails.age}</p>
       <p>your Height must be: {gameDetails.height}</p>
       <p>Maximum Weight: {gameDetails.weight}</p>
       <p>
-        we recomend that people who have medical issue for example: {gameDetails.midical_condition} to not try this game
+        we recomend that people who have medical issue for example:{" "}
+        {gameDetails.midical_condition} to not try this game
       </p>
     </div>
-  );
-};
+  )
+}
 
-export default GameDetails;
+export default GameDetails
