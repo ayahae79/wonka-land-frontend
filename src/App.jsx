98a22d167bb66eb7af5c41cae856ca01b5409729
aliHassanrabia <alihrabie@gmail.com>
2024-10-15 16:33:13 +0300

    update frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-
-import { useState, useEffect } from "react"
-import { Route, Routes } from "react-router"
-import { CheckSession } from "./services/Auth"
-import "./App.css"
-import HomePage from "./components/HomePage"
-import RegisterPage from "./pages/RegisterPage"
-import LoginPage from "./pages/LoginPage"
-import GameList from "./pages/GameList"
-import GameDetails from "./pages/GameDetails"
-import CreateGameForm from "./components/NewGame"
-
+// src/App.jsx
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router'
+import './App.css'
+import HomePage from './components/HomePage'
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
+import GameList from './pages/GameList'
+import GameDetails from './pages/GameDetails'
+import CreateGameForm from './components/NewGame'
+import { CheckSession } from './services/Auth'
 
 const App = () => {
   const [user, setUser] = useState(null)
 
   const handleLogOut = () => {
-    // Reset all auth related state and clear localStorage
     setUser(null)
     localStorage.clear()
   }
 
   const checkToken = async () => {
-    // If a token exists, sends token to localStorage to persist logged in user
     const user = await CheckSession()
     setUser(user)
   }
@@ -35,17 +32,16 @@ const App = () => {
   }, [])
 
   return (
-
     <div className="app-container">
       <div className="main-content">
-             <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage setUser={setUser} />} />
-        <Route path="/games" element={<GameList />} />
-        <Route path="/games/:Id" element={<GameDetails />} />
-        <Route path="games/creategame" element={<CreateGameForm />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage setUser={setUser} />} />
+          <Route path="/games" element={<GameList />} />
+          <Route path="/games/:Id" element={<GameDetails />} />
+          <Route path="/games/creategame" element={<CreateGameForm />} />
+        </Routes>
       </div>
       <div className="footer">Wonka Land &copy; </div>
     </div>
