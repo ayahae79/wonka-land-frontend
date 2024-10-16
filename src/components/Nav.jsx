46de25b35ac7441555a063f2c243e853b7ddf2af
Ayah Ahmed <ayahahmed729@gmail.com>
2024-10-16 04:15:53 +0300

    nav fixing

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,40 +1,86 @@
 import { Link } from "react-router-dom"
+import { FaHome, FaPowerOff, FaGamepad, FaPlus } from "react-icons/fa"
+import { RiLoginBoxLine, RiUserAddLine } from "react-icons/ri"
 
 const Nav = ({ user, handleLogOut }) => {
   const isAdmin = user && user.role === "admin"
 
   const adminOptions = (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link onClick={handleLogOut} to="/">
-        Sign Out
-      </Link>
-      <Link to="/games">Game List</Link>
-      <Link to="/games/creategame">Add Games</Link>
+    <nav className="nav">
+      <ul className="nav-list">
+        <li>
+          <Link to="/" className="nav-link">
+            <FaHome /> Home
+          </Link>
+        </li>
+        <li>
+          <Link onClick={handleLogOut} to="/" className="nav-link">
+            <FaPowerOff /> Sign Out
+          </Link>
+        </li>
+        <li>
+          <Link to="/games" className="nav-link">
+            <FaGamepad /> Game List
+          </Link>
+        </li>
+        <li>
+          <Link to="/games/creategame" className="nav-link">
+            <FaPlus /> Add Games
+          </Link>
+        </li>
+      </ul>
     </nav>
   )
 
   const userOptions = (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link onClick={handleLogOut} to="/">
-        Sign Out
-      </Link>
-      <Link to="/games">Game List</Link>
+    <nav className="nav">
+      <ul className="nav-list">
+        <li>
+          <Link to="/" className="nav-link">
+            <FaHome /> Home
+          </Link>
+        </li>
+        <li>
+          <Link onClick={handleLogOut} to="/" className="nav-link">
+            <FaPowerOff /> Sign Out
+          </Link>
+        </li>
+        <li>
+          <Link to="/games" className="nav-link">
+            <FaGamepad /> Game List
+          </Link>
+        </li>
+      </ul>
     </nav>
   )
 
   const publicOptions = (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/login">Sign In</Link>
+    <nav className="nav">
+      <ul className="nav-list">
+        <li>
+          <Link to="/" className="nav-link">
+            <FaHome /> Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/register" className="nav-link">
+            <RiUserAddLine /> Register
+          </Link>
+        </li>
+        <li>
+          <Link to="/login" className="nav-link">
+            <RiLoginBoxLine /> Sign In
+          </Link>
+        </li>
+      </ul>
     </nav>
   )
 
   return (
     <header>
-      {isAdmin ? adminOptions : user ? userOptions : publicOptions}
+      <div className="nav-container">
+        {isAdmin ? adminOptions : user ? userOptions : publicOptions}
+      </div>
     </header>
   )
 }
